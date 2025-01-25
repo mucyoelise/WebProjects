bdb9ab9e9b7560b65d237aafac6cb1a3ce7517d6
mucyoelise <mucyo.bryant@gmail.com>
2025-01-25 20:39:51 +0200

    first-commit

---
+++ Legidco/scripts/src/index.ts
@@ -0,0 +1,9 @@
+function dropdown() {
+    let dropIcon = document.querySelector("#navbar-middle");
+    if (dropIcon.className==="navbar-middle"){
+        dropIcon.className = "drop-list";
+    }
+    else {
+        dropIcon.className ="navbar-middle";
+    }
+}
