625d3368b9be75e1d05150c23f0b0e26edbebd2d
mucyoelise <mucyo.bryant@gmail.com>
2025-02-09 14:59:19 +0200

    Enhancing codes

--- Legidco/scripts/out/index.js
+++ Legidco/scripts/out/index.js
@@ -1,5 +1,5 @@
 function dropdown() {
-    let dropIcon = document.querySelector("#navbar-middle");
+    var dropIcon = document.querySelector("#navbar-middle");
     if (dropIcon.className === "navbar-middle") {
         dropIcon.className = "drop-list";
     }
@@ -7,3 +7,10 @@ function dropdown() {
         dropIcon.className = "navbar-middle";
     }
 }
+var loginBtn = document.querySelector("#login-btn");
+var signupBtn = document.querySelector("#signup-btn");
+function createLoginForm() {
+    return window.location.href = "./registration/index.html";
+}
+loginBtn.addEventListener("click", createLoginForm);
+signupBtn.addEventListener("click", createLoginForm);
